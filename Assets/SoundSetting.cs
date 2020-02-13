a036455f39bdf89a02aba6b1dafbbf3e635bd32b
seyma <seymaocal.97@gmail.com>
2020-02-13 22:52:14 +0300

    artırılmış gerçeklik

---
+++ Assets/SoundSetting.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.EventSystems;
+using Vuforia;
+
+public class SoundSetting : MonoBehaviour
+{
+
+    public AudioClip ses;
+    public AudioSource source;
+
+    public string butonsesadi;
+
+    public void OnClicked(Button button)
+    {
+
+        sesCal();
+       
+    }
+    public void OnClicked2(Button button)
+    {
+       
+        sesCal();
+    }
+
+    public void OnClicked3(Button button)
+    {
+
+        sesCal();
+
+    }
+
+    private void sesCal()
+    {
+        if (source.isPlaying)
+        {
+            source.Stop();
+        }
+        string name = EventSystem.current.currentSelectedGameObject.name;
+        print(name);
+        ses = Resources.Load<AudioClip>("sound/" + name);
+        print(ses);
+        source.PlayOneShot(ses);
+
+    }
+
+   
+}
